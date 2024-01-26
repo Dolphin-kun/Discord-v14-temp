8bdc998438ff59e203894bb7be9881be8b8ae281
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-26 22:08:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 const http = require("http");
 const querystring = require("query-string");
 
-//GASでwakeさせること。
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+const app = express();
 
-http
-  .createServer(function(req, res) {
-    if (req.method == "POST") {
-      var data = "";
-      req.on("data", function(chunk) {
-        data += chunk;
-      });
-      req.on("end", function() {
-        if (!data) {
-          res.end("No post data");
-          return;
-        }
-        var dataObject = querystring.parse(data);
-        //console.log("post:" + dataObject.type);
-        if (dataObject.type == "wake") {
-          //console.log("Woke up in post");
-          res.end();
-          return;
-        }
-        res.end();
-      });
-    } else if (req.method == "GET") {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Discord Bot is Oprateing!");
+app.use(express.static(path.join(__dirname, 'pages')));
+
+app.post("/", (req, res) => {
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+  req.on("end", () => {
+    if (!data) {
+      res.end("No post data");
+      return;
     }
-  })
-  .listen(3000);
+    const dataObject = querystring.parse(data);
+    // console.log("post:" + dataObject.type);
+    if (dataObject.type == "wake") {
+      // console.log("Woke up in post");
+      res.end();
+      return;
+    }
+    res.end();
+  });
+});
+
+app.get("/", (req, res) => {
+  fs.readFile("./pages/home.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+})
+
+// サーバーを起動
+app.listen(3000, () => {
+  console.log(`サーバーが開きました https://dolphin-kun.glitch.me`);
+});
 
 if (process.env.TOKEN == undefined || process.env.TOKEN == "") {
-  console.log("TOKENを設定してください。");
+  console.log("TOKENを設定してください");
   process.exit(0);
 }
 
